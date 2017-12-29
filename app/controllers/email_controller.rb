a6362996a7b0d20414e88b4940071d02f2b74a77
Rishi Dhar <rishidhar@Rishis-MacBook-Pro.local>
2017-12-29 15:16:19 -0800

    adding more checks and validations

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -5,43 +5,34 @@ class EmailController < ApplicationController
 	skip_before_action :verify_authenticity_token 
 
   def sendemail
-		
-		begin
-			# Expected body to POST
-			#{ to: ‘myemail@example.com’, subject: “hello”, body: “world” }
-			if params["subject"] == nil
-				render json: "Sending email without subject"
-			end
+		# Expected body to POST
+		#{ to: ‘myemail@example.com’, subject: “hello”, body: “world” }
+		if params["subject"].empty?
+			render json: {error: "Expecting subject in request"} and return
+		elsif params["body"].empty?
+			render json: { error: "Expecting Body in request" } and return
+		end
 
-			if params["body"] == nil
-				render json: "Sending email without any Body"
-			end
+		from = Email.new(email: 'rishidhar5788@gmail.com')
+	  subject = params["subject"]
+	  to = Email.new(email: params["to"])
+	  content = Content.new(type: 'text/plain', value: params["body"])
+	  mail = Mail.new(from, subject, to, content)
 
-			if params[:to] != nil
-				from = Email.new(email: 'rishidhar5788@gmail.com')
-			  subject = params["subject"]
-			  to = Email.new(email: params["to"])
-			  content = Content.new(type: 'text/plain', value: params["body"])
-			  mail = Mail.new(from, subject, to, content)
-			  puts mail.to_json
-
-				sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-				response = sg.client.mail._("send").post(request_body: mail.to_json)
-				p response.status_code
-				p response.body
-				p response.headers
-			else
-				render json: {
-					ERROR: {
-						status_code: response.status_code,
-						error: response.body
-						}
+		if params[:to] != nil
+			sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		else
+			response = sg.client.mail._("send").post(request_body: mail.to_json)
+			p mail.to_json
+			p response.status_code
+			p response.body
+			p response.headers
+			render json: {
+				ERROR: {
+					status_code: response.status_code,
+					error: response.body
 					}
-			end
-		rescue Exception => e
-			p "Something went wrong with the send, trying to resend email again in 5 mins" + e.to_s
-			sleep(5.minutes)
-			sendemail
+				}
 		end
   end
 end
