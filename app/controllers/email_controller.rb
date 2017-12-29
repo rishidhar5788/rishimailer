47a0277acb8dcd4c8f638afd096f0067410a7ec1
Rishi Dhar <rishidhar@Rishis-MacBook-Pro.local>
2017-12-29 15:33:25 -0800

    more error handling

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -8,9 +8,17 @@ class EmailController < ApplicationController
 		# Expected body to POST
 		#{ to: ‘myemail@example.com’, subject: “hello”, body: “world” }
 		if params["subject"].empty?
-			render json: {error: "Expecting subject in request"} and return
+			render json: {
+				error: "Expecting subject in request"
+				} and return
 		elsif params["body"].empty?
-			render json: { error: "Expecting Body in request" } and return
+			render json: {
+				error: "Expecting Body in request"
+				} and return
+		elsif params["to"].empty?
+			render json: {
+				error: "Can't continue without to address in request"
+				} and return
 		end
 
 		from = Email.new(email: 'rishidhar5788@gmail.com')
@@ -19,20 +27,18 @@ class EmailController < ApplicationController
 	  content = Content.new(type: 'text/plain', value: params["body"])
 	  mail = Mail.new(from, subject, to, content)
 
-		if !params[:to].empty?
-			sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-		else
+		begin
+			sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'].to_s)
 			response = sg.client.mail._("send").post(request_body: mail.to_json)
 			p mail.to_json
 			p response.status_code
 			p response.body
 			p response.headers
+		rescue Exception => e
 			render json: {
-				ERROR: {
-					status_code: response.status_code,
-					error: response.body
-					}
-				}
+				message: "Something went wrong with the send", 
+				error: e.to_s
+			}
 		end
   end
 end
