4ed8498e1caddfd04f48d8b7be3aeaf384525a04
Rishi Dhar <rishidhar@Rishis-MacBook-Pro.local>
2017-12-29 12:29:12 -0800

    testing with post body

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -5,30 +5,18 @@ class EmailController < ApplicationController
 	skip_before_action :verify_authenticity_token 
 
   def sendemail
-		data = JSON.parse('{
-		  "personalizations": [
-		    {
-		      "to": [
-		        {
-		          "email": "rishidhar5788@gmail.com"
-		        }
-		      ],
-		      "subject": "Sending with SendGrid is Fun"
-		    }
-		  ],
-		  "from": {
-		    "email": "rishidhar5788@gmail.com"
-		  },
-		  "content": [
-		    {
-		      "type": "text/plain",
-		      "value": "and easy to do anywhere, even with Ruby"
-		    }
-		  ]
-		}')
+		# Expected body to POST
+		#{ to: ‘myemail@example.com’, subject: “hello”, body: “world” }
+		
+	  from = Email.new(email: 'rishidhar5788@gmail.com')
+	  subject = params["subject"]
+	  to = Email.new(email: params["to"])
+	  content = Content.new(type: 'text/plain', value: params["body"])
+	  mail = Mail.new(from, subject, to, content)
+	  puts mail.to_json
+
 		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 		response = sg.client.mail._("send").post(request_body: data)
-		p response
 		p response.status_code
 		p response.body
 		p response.headers
